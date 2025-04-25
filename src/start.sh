f4c9296501af7d1f6d5808e8216362e451a44470
Tim Pietrusky <timpietrusky@gmail.com>
2025-04-25 15:46:32 +0200

    feat: use flux.1-dev-fp8 as default model; added conventions; improved start

--- src/start.sh
+++ src/start.sh
@@ -7,14 +7,14 @@ export LD_PRELOAD="${TCMALLOC}"
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "worker-comfyui: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+    python /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
 
     echo "worker-comfyui: Starting RunPod Handler"
-    python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
+    python -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
     echo "worker-comfyui: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
+    python /comfyui/main.py --disable-auto-launch --disable-metadata &
 
     echo "worker-comfyui: Starting RunPod Handler"
-    python3 -u /rp_handler.py
+    python -u /rp_handler.py
 fi
